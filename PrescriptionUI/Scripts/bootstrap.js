be450c3ec5e45633ca9b5833b6a180d045cc9698
Your Name <chanaleindig2@gmail.com>
2020-10-19 18:40:15 +0300

    chanele

--- PrescriptionUI/Scripts/bootstrap.js
+++ PrescriptionUI/Scripts/bootstrap.js
@@ -16,6 +16,15 @@ if (typeof jQuery === 'undefined') {
   }
 }(jQuery);
 
+
+
+// Data Picker Initialization
+$('.datepicker').datepicker({
+    inline: true;
+});
+
+
+
 /* ========================================================================
  * Bootstrap: transition.js v3.4.1
  * https://getbootstrap.com/docs/3.4/javascript/#transitions
